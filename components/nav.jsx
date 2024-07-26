f436dca262087c5f9f385c069ebca296dbaf30a6
Rodrigo745 <roodrigoquinteros@gmail.com>
2024-07-26 20:49:54 -0300

    nav btn

--- components/nav.jsx
+++ components/nav.jsx
@@ -2,9 +2,15 @@ import Image from "next/image"
 
 export default function Nav(){
     return(
-        <nav className="h-16 w-full bg-white text-slate-950 flex justify-between items-center px-10">
+        <nav className="h-[4vw] w-full bg-[#07F4B0] text-slate-950 flex justify-between items-center px-10">
             <div>
                 <Image src="/logo.png" width={50} height={50} alt="logo" />
+            </div>
+            <div className="text-slate-950 font-bold flex space-x-20 text-lg">
+                <p className="text-shadow-lg">Inicio</p>
+                <p>Planes</p>
+                <p>Contacto</p>
+                <button className="py-1 px-4 bg-white shadow-sm rounded-md">Ingresar</button>
             </div>
         </nav>
     )
