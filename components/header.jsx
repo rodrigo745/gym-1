df4d8e54093573de8cda9df030142728edd95d39
Rodrigo745 <roodrigoquinteros@gmail.com>
2024-08-09 19:50:14 -0300

    responsive nav

--- components/header.jsx
+++ components/header.jsx
@@ -1,8 +1,8 @@
 export default function Header(){
     return(
-        <header>
-            <div className="text-[#272E36]">
-                <h1 className="text-6xl font-bold text-shadow">Creador de <span className="text-[#fff]">CV</span></h1>
+        <header className="px-10">
+            <div className="text-[#272E36] pt-14">
+                <h1 className="text-4xl md:text-8xl font-bold text-shadow">Creador de <span className="text-[#fff]">CV</span></h1>
             </div>
             <div>
 
--- components/nav.jsx
+++ components/nav.jsx
@@ -2,8 +2,8 @@ import Image from "next/image"
 
 export default function Nav(){
     return(
-        <nav className="h-[4vw] w-full bg-[#272E36] text-[#9095A1] flex justify-end items-center px-10">
-            <div className="font-bold flex space-x-20 text-lg">
+        <nav className="h-[60px] w-full bg-[#272E36] text-[#9095A1] flex justify-end items-center px-10">
+            <div className="font-bold flex w-full md:w-fit justify-between md:space-x-20 text-xs md:text-lg">
                 <p className="text-shadow-lg">Inicio</p>
                 <p>Editor</p>
                 <p>Tutorial</p>
